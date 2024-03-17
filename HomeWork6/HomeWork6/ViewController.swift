9cc5430c537c0d66ace0d10e0f4527d551df848f
Kater8 <161327401+Kater8@users.noreply.github.com>
2024-03-17 22:33:07 +0200

    storyboard

--- HomeWork6/HomeWork6/ViewController.swift
+++ HomeWork6/HomeWork6/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buyCreditButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+//        buyCreditButton.layer.cornerRadius = 25.0
+//        buyCreditButton.layer.borderWidth = 1.0
+        buyCreditButton.layer.borderColor = UIColor.gray.cgColor
     }
 
 
